d7f6e978a48509a1b8a414afb4da5b5b05ed9856
gryde23 <126186165+gryde23@users.noreply.github.com>
2024-09-11 23:19:01 +0300

    накидал макет лабы

---
+++ src/main/java/com/example/jfx_lab1/HelloApplication.java
@@ -0,0 +1,22 @@
+package com.example.jfx_lab1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXMLS/hello-view.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
